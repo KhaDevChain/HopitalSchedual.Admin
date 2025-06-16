707ba82741c9e67a78e4cb8460c4e9920bf9a849
khann.devops <khann.devops.159@gmail.com>
2025-06-16 14:02:20 +0700

    Hủy bỏ sử dụng Redux và cấu trúc của nó

--- src/app/reducers.ts
+++
@@ -1,9 +0,0 @@
-import appSlice from "@/slice/app.slice";
-import signinSlice from "@/slice/signin.slice";
-import { combineReducers } from "@reduxjs/toolkit";
-
-const rootReducer = combineReducers({
-    app: appSlice,
-    signin: signinSlice
-  });
-  export default rootReducer;
--- src/slice/state.ts
+++
@@ -1,7 +0,0 @@
-import { actions } from "@/types";
-
-export interface BasicSliceState {
-  status: "idle" | "loading" | "completed" | "failed";
-  error?: string;
-  action: actions;
-}
